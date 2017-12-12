e3b7ab0869cc9260e7f9b634fd1616275f0d85dd
Rajashekhar <rajashekhar.bhairi@softdotcom.in>
2017-12-12 16:24:37 +0530

    analutics images

---
+++ src/pages/lab-report-test-parameters/lab-report-test-parameters.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { LabReportTestParametersPage } from './lab-report-test-parameters';
+
+@NgModule({
+  declarations: [
+    LabReportTestParametersPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(LabReportTestParametersPage),
+  ],
+})
+export class LabReportTestParametersPageModule {}
